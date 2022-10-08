f65621cfb47132bbfab52a3a3de39396a26a2bf4
a-cole55 <arniqua_c@yahoo.com>
2022-10-08 07:53:26 -0500

    initial commit

---
+++ src/components/City.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function City() {
+  return (
+    <div>
+        <h2>Name of City</h2>
+        <span>Temp</span>
+        <span>Cloudy, Sunny, etc</span>
+    </div>
+  )
+}
+
+export default City
