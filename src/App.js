5873f76dbdf62ffa03aa615fbe5716919996dd2f
a-cole55 <arniqua_c@yahoo.com>
2022-10-10 10:24:46 -0500

    added UseEffect Hook to fetch weather data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import City from "./components/City";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-import "./App.css"
 
 function App() {
+
+  useEffect(() => {
+    //Major City Zip Codes
+    const cityZips = [78759, 77005, 75201];
+
+    //Get API Data
+    const getWeatherData = async () =>{
+      try {
+        //Insert each zipcode into API URL; creating an array of the zipcode urls
+          const cityZipURL = await cityZips.map((ele) => {
+            return(`https://api.openweathermap.org/data/2.5/weather?zip=${ele}&appid=0d923a011b9d8bfcfb3fd85c0c2dfb8d&units=imperial`)
+          });
+
+          //Fetch API data for each city
+          const getWeatherData = await Promise.all(cityZipURL.map((url) => axios.get(url)))
+            .then(
+              axios.spread((...allData) => {
+              console.log({ allData });
+              let weatherData = allData.map((data) =>{
+                return data.data
+              });
+              console.log(weatherData);
+              // setUserInfo(userData)
+      })
+      );
+        } catch (error){
+        console.log(error)
+      }
+    }
+      getWeatherData();
+
+
+}, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
